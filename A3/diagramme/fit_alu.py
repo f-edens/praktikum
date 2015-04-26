16881e9c40601e219cc59783c43a964a25994489
Frederik Edens <f_eden01@wwu.de>
2015-04-26 21:54:19 +0200

    skript zum fitten angepasst an Pb

--- A3/diagramme/fit_alu.py
+++ A3/diagramme/fit_alu.py
@@ -6,15 +6,22 @@ from io import BytesIO
 
 from scipy.optimize import leastsq as lsq
 
-data = """0	10.7211538462	0.3372167807
-50	10.045045045	0.3141423395
-107	10.0267857143	0.3123132646
-165	8.3759398496	0.2587336329
-215	7.0891719745	0.2172391352
-330	6.4540229885	0.1961323762
-520	5.3142857143	0.1610792422
-770	3.9363957597	0.1187560548"""
+data = """0	10.4791248606	0.3480304704
+50	9.8030160595	0.3249560292
+107	9.7847567288	0.3231269543
+165	8.1339108641	0.2695473226
+215	6.847142989	0.228052825
+330	6.211994003	0.206946066
+520	5.0722567288	0.171892932
+770	3.6943667742	0.1295697446"""
 
+
+data_pb = """0	8.9811115104	0.2972302496
+5	5.0032540334	0.1700432809
+10	3.0248331846	0.1091601283
+15	1.8822953388	0.0727996721
+20	1.0670191931	0.0500643354
+"""
 
 def logistic_fun(x, n_0, x_0, a):
     return n_0*(1+np.exp(-a*x_0))/(1 + np.exp(a*(x-x_0)))
@@ -23,13 +30,14 @@ def logistic_fun(x, n_0, x_0, a):
 def expo_fun(x, n_0, a):
     return n_0 * np.exp(a*x)
 
+
 def main():
-    npdata = np.genfromtxt(BytesIO(data.encode()))
+    npdata = np.genfromtxt(BytesIO(data_pb.encode()))
     data_x, data_y = npdata[:, 0], npdata[:, 1]
-    data_x *= 1e-6
-    ps, es = optimize.curve_fit(logistic_fun, data_x, data_y)
-    print(ps, "\n", es)
-    print()
+    #data_x *= 1e-6
+    # ps, es = optimize.curve_fit(logistic_fun, data_x, data_y)
+    # print(ps, "\n", es)
+    # print()
     ps, es = optimize.curve_fit(expo_fun, data_x, data_y)
     print(ps, "\n", es)
     pass
