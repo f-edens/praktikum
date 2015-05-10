85d126180e23768479f0aea76d9026834272d666
Frederik Edens <f_eden01@wwu.de>
2015-05-10 02:22:07 +0200

    kleine Erweiterung zur python-Auswertung

--- pythonUtils/data.py
+++ pythonUtils/data.py
@@ -3,11 +3,23 @@ __author__ = 'fred'
 import numpy as np
 
 
+def komma_to_float(input):
+    return float(input.replace(",", "."))
+
+
+col_name_xy = ("x", "y")
+col_name_xyerr = ("x", "y", "err")
+
+
 class Data:
-    def __init__(self, indata, col_names=("x", "y", "xerr", "yerr"), skip_lines=0):
+    def __init__(self, indata, col_names=("x", "y", "xerr", "yerr"), skip_lines=0, converters=None):
         self.col_names = {name: i for i, name in enumerate(col_names)}
 
-        self.data_source = np.genfromtxt(indata, skiprows=skip_lines)
+        if not converters:
+            self.data_source = np.genfromtxt(indata, skiprows=skip_lines,
+                                             converters=[komma_to_float for _ in col_names])
+        else:
+            self.data_source = np.genfromtxt(indata, skiprows=skip_lines, converters=converters)
 
     def __getattr__(self, item):
         if item in self.col_names:
