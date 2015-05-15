3ce212af55c72331578b05af3cfef57ceb8f8e9a
Frederik Edens <f_eden01@wwu.de>
2015-05-15 21:41:17 +0200

    W5 diverse Diagramme und Tabellen

---
+++ W5/auswertung/versuch1.py
@@ -0,0 +1,23 @@
+__author__ = 'fred'
+
+from scipy import optimize as opt
+import numpy as np
+
+from pythonUtils import data
+
+input_file = "co2.data"
+
+
+def main():
+    dat = data.Data(input_file, col_names=["x", "xerr", "y", "yerr"])
+    x = dat.x
+    y = dat.y
+    fits, errs = opt.curve_fit(lambda x, a, b: a * x + b, x, y)
+    print(str(np.asmatrix(errs) * np.asmatrix(fits).transpose()))
+    with open(input_file + ".fit", "w+") as out_file:
+        out_file.write(str(fits) + "\n")
+        out_file.write(str(errs))
+
+
+if __name__ == '__main__':
+    main()
